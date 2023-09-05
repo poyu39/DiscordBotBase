2843305b4f377665a5f45d596e0f2041d13e06a5
Poyu <42506064+poyu39@users.noreply.github.com>
2023-09-05 23:21:58 +0800

    feat: 新增擁有者權限點

--- src/cogs/admin/ext_control.py
+++ src/cogs/admin/ext_control.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-from settings import Config, logger
+from settings import Config, logger, replier
 
 CONFIG = Config()
 
@@ -17,23 +17,36 @@ class ExtControl(commands.Cog):
     @commands.command()
     async def un(self, ctx, extension):
         if ctx.author.id != CONFIG.OWNER_ID:
-            await ctx.send('此指令僅限擁有者使用')
-            return
-        await self.bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(f'unloaded {extension} done')
+            reply_embed = replier.error(value='此指令僅限擁有者使用')
+            await ctx.send(embed=reply_embed)
+        else:
+            await self.bot.unload_extension(f'cogs.{extension}')
+            logger.info(f'unloaded {extension} done')
+            reply_embed = replier.success(value=f'已卸載 {extension}')
+            await ctx.send(embed=reply_embed)
 
     @commands.command()
     async def re(self, ctx, extension):
-        await self.bot.reload_extension(f'cogs.{extension}')
-        await self.bot.tree.sync(guild=discord.Object(id=CONFIG.GUILD_ID))
-        logger.info(f'reloaded {extension} done')
-        await ctx.send(f'reloaded {extension} done')
+        if ctx.author.id != CONFIG.OWNER_ID:
+            reply_embed = replier.error(value='此指令僅限擁有者使用')
+            await ctx.send(embed=reply_embed)
+        else:
+            await self.bot.reload_extension(f'cogs.{extension}')
+            await self.bot.tree.sync(guild=discord.Object(id=CONFIG.GUILD_ID))
+            logger.info(f'reloaded {extension} done')
+            reply_embed = replier.success(value=f'已重新載入 {extension}')
+            await ctx.send(embed=reply_embed)
 
     @commands.command()
     async def l(self, ctx, extension):
-        await self.bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f'loaded {extension} done')
-
+        if ctx.author.id != CONFIG.OWNER_ID:
+            reply_embed = replier.error(value='此指令僅限擁有者使用')
+            await ctx.send(embed=reply_embed)
+        else:
+            await self.bot.load_extension(f'cogs.{extension}')
+            logger.info(f'loaded {extension} done')
+            reply_embed = replier.success(value=f'已載入 {extension}')
+            await ctx.send(embed=reply_embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(ExtControl(bot), guild=bot.get_guild(CONFIG.GUILD_ID))
